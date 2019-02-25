2adb80eb7d5b9c5cff4bfd2c84fd198e8ced5dca
Vinicius Amaral <vcamaral@gmail.com>
2019-02-24 22:03:09 -0300

    Implements retry police.

---
+++ src/Polly.Retry/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Polly.Retry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Only for tests.
+            Retry().GetAwaiter().GetResult();
+
+            Console.ReadKey();
+        }
+
+        static async Task Retry()
+        {
+            var retryPolicy = Policy.Handle<Exception>()
+                .WaitAndRetryAsync(retryCount: 5, 
+                                   sleepDurationProvider: retryAgain => TimeSpan.FromSeconds(1));
+
+            try
+            {
+                await retryPolicy.ExecuteAsync(async () =>
+                {
+                    // Request invalid URL to simulate retry.
+                    await Request("foo.bar");
+                });
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Retry max attempts exceeded.");
+
+                // Request real URL to ensure success.
+                await Request("http://httpbin.org/get");
+            }
+        }
+
+        static async Task Request(string url)
+        {
+            Console.WriteLine($"Request URL: {url}");
+
+            var client = new HttpClient();
+            var response = await client.GetStringAsync(url);
+
+            Console.WriteLine($"Response: {response}");
+        }
+    }
+}
